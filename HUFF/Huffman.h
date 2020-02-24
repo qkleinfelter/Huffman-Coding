89cab6c412ad7904662ccaac3aa693638b2dcb43
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-02-24 14:20:49 -0500

    Basic setup of huffman class

--- HUFF/Huffman.h
+++ HUFF/Huffman.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <string>
+using namespace std;
+
+class Huffman
+{
+public:
+	void MakeTreeBuilder(string inputFile, string outputFile);
+	void EncodeFile(string inputFile, string outputFile);
+	void DecodeFile(string inputFile, string outputFile);
+	void EncodeFileWithTree(string inputFile, string treeFile, string outputFile);
+	void DisplayHelp();
+
+private:
+	struct node 
+	{
+		char symb;
+		int weight;
+		node* left;
+		node* right;
+	};
+};
